a0c202295c88ff5dc1d1ea2d074a7115a1774358
Riven Skaye <riven@tae.moe>
2022-12-02 00:25:14 +0100

    cleanup

--- 2022/src/bin/day1.rs
+++ 2022/src/bin/day1.rs
@@ -3,21 +3,16 @@ extern crate test;
 
 use aoc2022::common;
 
-pub fn get_data<'a>(data: &'a String) -> impl Iterator<Item = usize> + 'a {
-    data.split("\n\n").map(|inv| {
-        inv.lines()
-            .map(|item| unsafe { usize::from_str_radix(item, 10).unwrap_unchecked() })
-            .sum::<usize>()
-    })
+pub fn get_data(data: &str) -> Vec<usize> {
+    data.split("\n\n")
+        .map(|inv| {
+            inv.lines()
+                .map(|item| unsafe { item.parse::<usize>().unwrap_unchecked() })
+                .sum::<usize>()
+        })
+        .collect()
 }
-
-#[cfg(not(test))]
-pub fn part_one<'a>(data: impl Iterator<Item = usize> + 'a) -> usize {
-    data.max().unwrap()
-}
-
-#[cfg(test)]
-pub fn part_one(data: Vec<usize>) -> usize {
+pub fn part_one(data: &Vec<usize>) -> usize {
     *data.iter().max().unwrap()
 }
 
@@ -28,10 +23,9 @@ pub fn part_two(mut data: Vec<usize>) -> usize {
 
 pub fn main() {
     let data = common::read_file(1);
-    //let parsed = get_data(&data);
-    //println!("Part one: {}", part_one(parsed));
     let parsed = get_data(&data);
-    println!("Part two: {}", part_two(parsed.collect()));
+    part_one(&parsed);
+    println!("Part two: {}", part_two(parsed));
 }
 
 #[cfg(test)]
@@ -47,14 +41,14 @@ mod aoc_benching {
     #[bench]
     fn part1bench(b: &mut test::Bencher) {
         let read = common::read_file(1);
-        let input = get_data(&read).collect::<Vec<usize>>();
-        b.iter(|| part_one(input.clone()))
+        let input = get_data(&read);
+        b.iter(|| part_one(test::black_box(&input)))
     }
 
     #[bench]
     fn part2bench(b: &mut test::Bencher) {
         let read = common::read_file(1);
-        let input = get_data(&read).collect::<Vec<usize>>();
+        let input = get_data(&read);
         b.iter(|| part_two(test::black_box(input.clone())))
     }
 }
