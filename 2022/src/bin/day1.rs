a0c2022df0d79ca5f29828e3275ce0836c7e6761
Riven Skaye <riven@tae.moe>
2022-12-01 16:10:11 +0100

    Cleanup, prep for benching

--- 2022/src/bin/day1.rs
+++ 2022/src/bin/day1.rs
@@ -1,33 +1,27 @@
 use aoc2022::common;
 
-pub fn part_one() -> usize {
+pub fn get_data() -> Vec<usize> {
     common::read_file(1)
         .split("\n\n")
         .map(|inv| {
             inv.lines()
-                .map(|item| usize::from_str_radix(item, 10).unwrap_or(0))
-                .sum()
+                .map(|item| unsafe { usize::from_str_radix(item, 10).unwrap_unchecked() })
+                .sum::<usize>()
         })
-        .max()
-        .unwrap_or(0)
+        .collect()
 }
 
-pub fn part_two() -> usize {
-    let mut cals = common::read_file(1)
-        .split("\n\n")
-        .map(|inv| {
-            inv.lines()
-                .map(|item| usize::from_str_radix(item, 10).unwrap_or(0))
-                .sum()
-        })
-        .collect::<Vec<usize>>();
-    cals.sort();
-    cals.reverse();
-    cals.truncate(3);
-    cals.iter().sum()
+pub fn part_one(data: &Vec<usize>) -> usize {
+    unsafe { *data.iter().max().unwrap_unchecked() }
+}
+
+pub fn part_two(mut data: Vec<usize>) -> usize {
+    data.sort();
+    data.iter().rev().take(3).sum()
 }
 
 pub fn main() {
-    println!("Part one: {}", part_one());
-    println!("Part two: {}", part_two());
+    let data = get_data();
+    println!("Part one: {}", part_one(&data));
+    println!("Part two: {}", part_two(data));
 }
