a0c2022709f98b9ee28646f8e59114a9213f943b
Riven Skaye <riven@tae.moe>
2022-12-02 10:11:30 +0100

    I am speed

--- 2022/src/bin/day2.rs
+++ 2022/src/bin/day2.rs
@@ -1,21 +1,17 @@
 #![feature(test)]
+#![feature(array_chunks)]
 extern crate test;
 
 use aoc2022::common;
 
-pub fn get_data(data: &str) -> Vec<(u8, u8)> {
-    data.lines()
-        .map(|line| {
-            let moves = line.as_bytes();
-            (moves[0], moves[2])
-        })
-        .collect()
+pub fn get_data(data: &str) -> &[u8] {
+    data.as_bytes()
 }
 
-pub fn part_one(input: Vec<(u8, u8)>) -> usize {
+pub fn part_one(input: &[u8]) -> usize {
     input
-        .iter()
-        .map(|round| match round {
+        .array_chunks::<4>()
+        .map(|round| match (round[0], round[2]) {
             (b'A', b'X') => 4,
             (b'A', b'Y') => 8,
             (b'A', b'Z') => 3,
@@ -30,10 +26,10 @@ pub fn part_one(input: Vec<(u8, u8)>) -> usize {
         .sum()
 }
 
-pub fn part_two(input: Vec<(u8, u8)>) -> usize {
+pub fn part_two(input: &[u8]) -> usize {
     input
-        .iter()
-        .map(|round| match round {
+        .array_chunks::<4>()
+        .map(|round| match (round[0], round[2]) {
             (b'A', b'X') => 3,
             (b'A', b'Y') => 4,
             (b'A', b'Z') => 8,
@@ -68,13 +64,13 @@ mod aoc_benching {
     fn part1bench(b: &mut test::Bencher) {
         let read = common::read_file(2);
         let input = get_data(&read);
-        b.iter(|| assert_eq!(part_one(test::black_box(input.clone())), 13565))
+        b.iter(|| assert_eq!(part_one(test::black_box(input)), 13565))
     }
 
     #[bench]
     fn part2bench(b: &mut test::Bencher) {
         let read = common::read_file(2);
         let input = get_data(&read);
-        b.iter(|| assert_eq!(part_two(test::black_box(input.clone())), 12424))
+        b.iter(|| assert_eq!(part_two(test::black_box(input)), 12424))
     }
 }
--- 2022/src/nox/mod.rs
+++ 2022/src/nox/mod.rs
@@ -1,1 +1,2 @@
 pub mod day1_nox;
+pub mod day2_nox;
