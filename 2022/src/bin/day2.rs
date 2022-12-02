a0c20225ca11729de623a86aa9f971a013d2e35c
Riven Skaye <riven@tae.moe>
2022-12-02 09:38:01 +0100

    add benchmarks

--- 2022/src/bin/day2.rs
+++ 2022/src/bin/day2.rs
@@ -3,9 +3,8 @@ extern crate test;
 
 use aoc2022::common;
 
-pub fn get_data() -> Vec<(u8, u8)> {
-    common::read_file(2)
-        .lines()
+pub fn get_data(data: &str) -> Vec<(u8, u8)> {
+    data.lines()
         .map(|line| {
             let moves = line.as_bytes();
             (moves[0], moves[2])
@@ -17,15 +16,15 @@ pub fn part_one(input: Vec<(u8, u8)>) -> usize {
     input
         .iter()
         .map(|round| match round {
-            (b'A', b'X') => 3 + 1,
-            (b'A', b'Y') => 6 + 2,
-            (b'A', b'Z') => 0 + 3,
-            (b'B', b'X') => 0 + 1,
-            (b'B', b'Y') => 3 + 2,
-            (b'B', b'Z') => 6 + 3,
-            (b'C', b'X') => 6 + 1,
-            (b'C', b'Y') => 0 + 2,
-            (b'C', b'Z') => 3 + 3,
+            (b'A', b'X') => 4,
+            (b'A', b'Y') => 8,
+            (b'A', b'Z') => 3,
+            (b'B', b'X') => 1,
+            (b'B', b'Y') => 5,
+            (b'B', b'Z') => 9,
+            (b'C', b'X') => 7,
+            (b'C', b'Y') => 2,
+            (b'C', b'Z') => 6,
             (_, _) => unreachable!(),
         })
         .sum()
@@ -35,21 +34,47 @@ pub fn part_two(input: Vec<(u8, u8)>) -> usize {
     input
         .iter()
         .map(|round| match round {
-            (b'A', b'X') => 3 + 0,
-            (b'A', b'Y') => 1 + 3,
-            (b'A', b'Z') => 2 + 6,
-            (b'B', b'X') => 1 + 0,
-            (b'B', b'Y') => 2 + 3,
-            (b'B', b'Z') => 3 + 6,
-            (b'C', b'X') => 2 + 0,
-            (b'C', b'Y') => 3 + 3,
-            (b'C', b'Z') => 1 + 6,
+            (b'A', b'X') => 3,
+            (b'A', b'Y') => 4,
+            (b'A', b'Z') => 8,
+            (b'B', b'X') => 1,
+            (b'B', b'Y') => 5,
+            (b'B', b'Z') => 9,
+            (b'C', b'X') => 2,
+            (b'C', b'Y') => 6,
+            (b'C', b'Z') => 7,
             (_, _) => unreachable!(),
         })
         .sum()
 }
 
 pub fn main() {
-    println!("{}", part_one(get_data()));
-    println!("{}", part_two(get_data()));
+    let input = common::read_file(2);
+    println!("{}", part_one(get_data(&input)));
+    println!("{}", part_two(get_data(&input)));
+}
+
+#[cfg(test)]
+mod aoc_benching {
+    use super::*;
+
+    #[bench]
+    fn parsebench(b: &mut test::Bencher) {
+        let input = common::read_file(2);
+        b.iter(|| get_data(test::black_box(&input)))
+    }
+
+    #[bench]
+    fn part1bench(b: &mut test::Bencher) {
+        let read = common::read_file(2);
+        let input = get_data(&read);
+        b.iter(|| part_one(test::black_box(input.clone())))
+    }
+
+    #[bench]
+    fn part2bench(b: &mut test::Bencher) {
+        let read = common::read_file(2);
+        let input = get_data(&read);
+        b.iter(|| part_two(test::black_box(input.clone())))
+    }
 }
