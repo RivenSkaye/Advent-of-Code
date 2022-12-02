a0c20224bab4b1ffe4c6944e21e9e13426ee14b0
Riven Skaye <riven@tae.moe>
2022-12-02 10:42:15 +0100

    More consistent speeds because ???

--- 2022/src/bin/day2.rs
+++ 2022/src/bin/day2.rs
@@ -11,17 +11,17 @@ pub fn get_data(data: &str) -> &[u8] {
 pub fn part_one(input: &[u8]) -> usize {
     input
         .array_chunks::<4>()
-        .map(|round| match (round[0], round[2]) {
-            (b'A', b'X') => 4,
-            (b'A', b'Y') => 8,
-            (b'A', b'Z') => 3,
-            (b'B', b'X') => 1,
-            (b'B', b'Y') => 5,
-            (b'B', b'Z') => 9,
-            (b'C', b'X') => 7,
-            (b'C', b'Y') => 2,
-            (b'C', b'Z') => 6,
-            (_, _) => unreachable!(),
+        .map(|round| match [round[0], round[2]] {
+            [b'A', b'X'] => 4,
+            [b'A', b'Y'] => 8,
+            [b'A', b'Z'] => 3,
+            [b'B', b'X'] => 1,
+            [b'B', b'Y'] => 5,
+            [b'B', b'Z'] => 9,
+            [b'C', b'X'] => 7,
+            [b'C', b'Y'] => 2,
+            [b'C', b'Z'] => 6,
+            _ => unreachable!(),
         })
         .sum()
 }
@@ -29,17 +29,17 @@ pub fn part_one(input: &[u8]) -> usize {
 pub fn part_two(input: &[u8]) -> usize {
     input
         .array_chunks::<4>()
-        .map(|round| match (round[0], round[2]) {
-            (b'A', b'X') => 3,
-            (b'A', b'Y') => 4,
-            (b'A', b'Z') => 8,
-            (b'B', b'X') => 1,
-            (b'B', b'Y') => 5,
-            (b'B', b'Z') => 9,
-            (b'C', b'X') => 2,
-            (b'C', b'Y') => 6,
-            (b'C', b'Z') => 7,
-            (_, _) => unreachable!(),
+        .map(|round| match [round[0], round[2]] {
+            [b'A', b'X'] => 3,
+            [b'A', b'Y'] => 4,
+            [b'A', b'Z'] => 8,
+            [b'B', b'X'] => 1,
+            [b'B', b'Y'] => 5,
+            [b'B', b'Z'] => 9,
+            [b'C', b'X'] => 2,
+            [b'C', b'Y'] => 6,
+            [b'C', b'Z'] => 7,
+            _ => unreachable!(),
         })
         .sum()
 }
