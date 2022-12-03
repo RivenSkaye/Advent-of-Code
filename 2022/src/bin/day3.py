a0c20224b26388864ef3b19849e5146a8265e156
Riven Skaye <riven@tae.moe>
2022-12-03 23:50:51 +0100

    Day 3 complete and done

---
+++ 2022/src/bin/day3.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# ord('a') is 97, 'a' is prio 1 => ord('a') - 96
+# ord('A') is 65, 'A' is prio 27 => ord('A') - (64 - 26)
+offset_lower = 96
+offset_upper = (64 - 26)
+
+
+def part_one(aoc_data):
+    dupval = 0
+
+    txt = aoc_data.splitlines()
+    for line in txt:
+        first = frozenset(line[:len(line) // 2])
+        second = frozenset(line[len(line) // 2:])
+        processed = []
+        for char in first:
+            if char in second and char not in processed:
+                dupval += ord(char) - (offset_lower if char.islower()
+                                       else offset_upper)
+                processed.append(char)
+
+    print(dupval)
+
+
+def part_two(aoc_data):
+    badgeval = 0
+
+    txt = aoc_data.splitlines()
+    for i in range(0, len(txt), 3):
+        elf1, elf2, elf3 = [frozenset(t) for t in txt[i:i+3]]
+        for char in elf1:
+            if char in elf2 and char in elf3:
+                badgeval += ord(char) - (offset_lower if char.islower()
+                                         else offset_upper)
+                continue
+
+    print(badgeval)
+
+
+if __name__ == "__main__":
+    import os
+    os.chdir(os.path.dirname(__file__))
+    f = open("../../inputs/03.txt")
+    aoc = f.read()
+    f.close()
+    part_one(aoc)
+    part_two(aoc)
