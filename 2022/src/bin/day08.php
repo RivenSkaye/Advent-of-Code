a0c202271fdf218c0b69a9fade05ccc899e2ee9f
Riven Skaye <riven@tae.moe>
2022-12-08 11:59:53 +0100

    Drop some comps, skip coercion

--- 2022/src/bin/day08.php
+++ 2022/src/bin/day08.php
@@ -23,11 +23,12 @@ function row_vis(int $idx, array $row): int
     $rev = array_reverse($row);
     $len = count($row);
     $revidx = ($len - $idx) - 1;
+    $over = $row[$idx] - 1;
     $from_left = $from_right = false;
     for ($i = 0; $i < $len; $i++) {
         // Stop checking either if it's true
-        $from_left = $from_left || ($i < $idx && $row[$i] >= $row[$idx]);
-        $from_right = $from_right || ($i < $revidx && $rev[$i] >= $row[$idx]);
+        $from_left = $from_left || ($i < $idx && $row[$i] > $over);
+        $from_right = $from_right || ($i < $revidx && $rev[$i] > $over);
         // Early exit from the loop once we know for sure
         if ($from_left && $from_right) return 1;
     }
@@ -53,7 +54,8 @@ for ($y = 0; $y <= $cols; $y++) {
     }
     // For everything in this column not on an edge, check visibility
     for ($x = 1; $x < $rows; $x++) {
-        if ($hidden[$x] === 1) $hidden[$x] = row_vis($x, $col);
+        $chk = &$hidden[$x];
+        if ($chk === 1) $chk = row_vis($x, $col);
     }
     $visible += array_sum($hidden);
 }
@@ -72,8 +74,8 @@ function row_score(int $idx, array $row): int
         $to_left += 1;
     }
     // Same in the other direction
-    for ($x = $idx + 1; $x <= $len; $x++) {
-        if ($row[$x] >= $row[$idx] || $x === $len) break;
+    for ($x = $idx + 1; $x < $len; $x++) {
+        if ($row[$x] >= $row[$idx]) break;
         $to_right += 1;
     }
     return $to_left * $to_right;
@@ -87,7 +89,7 @@ for ($y = 0; $y <= $cols; $y++) {
         $cur = $input[$x][$y];
         array_push($col, $cur);
         // Edges have one side without trees, so it always scores 0
-        if ($y == 0 || $y == $cols || $x == 0 || $x == $rows) {
+        if ($y === 0 || $y === $cols || $x === 0 || $x === $rows) {
             array_push($score, 0);
         } else {
             array_push($score, row_score($y, $input[$x]));
