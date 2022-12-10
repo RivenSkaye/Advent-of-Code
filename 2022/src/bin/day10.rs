a0c20220c3598d1577fdc9b6b64b1446becfc7e8
Riven Skaye <riven@tae.moe>
2022-12-10 22:17:56 +0100

    Much better :umu:

--- 2022/src/bin/day10.rs
+++ 2022/src/bin/day10.rs
@@ -1,50 +1,35 @@
 use aoc2022::common::read_file;
 
 pub fn parse(input: &str) -> Vec<(i64, i64)> {
+    let mut x_reg = 1;
     input
         .lines()
         .map(|line| match line.starts_with('a') {
-            true => (2, line.split_once(" ").unwrap().1.parse().unwrap()),
-            false => (1, 0),
+            true => {
+                let num: i64 = line.split_once(" ").unwrap().1.parse().unwrap();
+                x_reg += num;
+                (2, x_reg)
+            }
+            false => (1, x_reg),
         })
         .collect()
 }
 
-pub fn part_one<const END: i64>(input: &Vec<(i64, i64)>) -> i64 {
-    // start at 20
+pub fn part_one(input: &Vec<(i64, i64)>) -> i64 {
     let mut cycles = 0;
-    let mut x_reg = 1;
     let mut sigstrength = 0;
-    let mut last = 0;
-    let mut lastval = 0;
-    for (instruction, value) in input {
-        let comp = if last == 2 && cycles & 1 == 0 {
-            cycles
-        } else {
-            cycles + 1
-        };
-        match comp {
-            20 | 60 | 100 | 140 | 180 => {
-                sigstrength += if comp == cycles {
-                    x_reg - lastval
-                } else {
-                    x_reg
-                } * (comp);
-                println!("{cycles}:  {x_reg} * {} = {}", comp, x_reg * (comp))
-            }
+    for i in 0..input.len() {
+        let next = cycles + input[i].0;
+        match (cycles, next) {
+            (18 | 19, 20..) => sigstrength += input[i - 1].1 * 20,
+            (58 | 59, 60..) => sigstrength += input[i - 1].1 * 60,
+            (98 | 99, 100..) => sigstrength += input[i - 1].1 * 100,
+            (138 | 139, 140..) => sigstrength += input[i - 1].1 * 140,
+            (178 | 179, 180..) => sigstrength += input[i - 1].1 * 180,
+            (218 | 219, 220..) => sigstrength += input[i - 1].1 * 220,
             _ => (),
-        };
-        cycles += *instruction;
-        if cycles > END - 2 {
-            sigstrength += x_reg * END;
-            println!("{cycles}:  {x_reg} * {} = {}", END, x_reg * END);
-            break;
         }
-        if *instruction == 2 {
-            x_reg += *value;
-            lastval = *value;
-        }
-        last = *instruction;
+        cycles = next;
     }
     sigstrength
 }
@@ -52,5 +37,5 @@ pub fn part_one<const END: i64>(input: &Vec<(i64, i64)>) -> i64 {
 pub fn main() {
     let data = read_file::<10>();
     let parsed = parse(&data);
-    println!("Part one: {}", part_one::<220>(&parsed));
+    println!("Part one: {}", part_one(&parsed));
 }
