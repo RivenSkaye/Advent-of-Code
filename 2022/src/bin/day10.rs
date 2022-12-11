a0c2022eaf075b1659b3e57d474a69511e26c45b
Riven Skaye <riven@tae.moe>
2022-12-11 02:19:45 +0100

    p2 done. I swear these benchmarks are off

--- 2022/src/bin/day10.rs
+++ 2022/src/bin/day10.rs
@@ -1,3 +1,4 @@
+#![feature(test)]
 use aoc2022::common::read_file;
 
 pub fn parse(input: &str) -> Vec<(i64, i64)> {
@@ -6,7 +7,8 @@ pub fn parse(input: &str) -> Vec<(i64, i64)> {
         .lines()
         .map(|line| match line.starts_with('a') {
             true => {
-                let num: i64 = line.split_once(" ").unwrap().1.parse().unwrap();
+                let num: i64 =
+                    unsafe { line.split_once(" ").unwrap().1.parse().unwrap_unchecked() };
                 x_reg += num;
                 (2, x_reg)
             }
@@ -34,8 +36,61 @@ pub fn part_one(input: &Vec<(i64, i64)>) -> i64 {
     sigstrength
 }
 
+pub fn part_two(input: &Vec<(i64, i64)>) -> String {
+    let mut cycles = 0;
+    let mut x_reg = 0_i64;
+    let mut output = String::with_capacity(240);
+    input.iter().for_each(|(steps, res)| {
+        for i in cycles..cycles + steps {
+            output.push(if x_reg.abs_diff(if i < 40 { i } else { 0 }) < 2 {
+                '#'
+            } else {
+                '.'
+            });
+        }
+        x_reg = *res;
+        cycles = (cycles + steps) % 40;
+    });
+    output
+}
+
 pub fn main() {
     let data = read_file::<10>();
     let parsed = parse(&data);
     println!("Part one: {}", part_one(&parsed));
+    let mut i = 1;
+    println!("");
+    for c in part_two(&parsed).chars() {
+        print!("{c}");
+        if i == 0 {
+            println!("");
+        }
+        i = (i + 1) % 40;
+    }
+}
+
+#[cfg(test)]
+mod aoc_benching {
+    extern crate test;
+    use super::*;
+
+    #[bench]
+    fn parsebench(b: &mut test::Bencher) {
+        let input = read_file::<10>();
+        b.iter(|| parse(test::black_box(&input)))
+    }
+
+    #[bench]
+    fn part1bench(b: &mut test::Bencher) {
+        let read = read_file::<10>();
+        let input = parse(&read);
+        b.iter(|| assert_eq!(part_one(test::black_box(&input)), 13440))
+    }
+
+    #[bench]
+    fn part2bench(b: &mut test::Bencher) {
+        let read = read_file::<10>();
+        let input = parse(&read);
+        b.iter(|| assert_eq!(part_two(test::black_box(&input)), "###..###..####..##..###...##..####..##..#..#.#..#....#.#..#.#..#.#..#....#.#..#.#..#.###....#..#....#..#.#..#...#..#..#.###..#..#..#...#.##.###..####..#...####.#....#..#.#....#..#.#.#..#..#.#....#..#.#....###..####..###.#..#.#..#.####.#..#."))
+    }
 }
