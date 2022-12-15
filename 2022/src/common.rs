a0c2022794d2257611d0ed21f10f372bce0eaa66
Riven Skaye <riven@tae.moe>
2022-12-15 11:48:27 +0100

    This can't hurt to have in a central place

--- 2022/src/common.rs
+++ 2022/src/common.rs
@@ -8,3 +8,12 @@ pub fn read_file<const DAY: usize>() -> String {
     println!("Reading `{FILENAME}`");
     unsafe { std::fs::read_to_string(format!("{FILENAME}/{DAY:0>2}.txt")).unwrap_unchecked() }
 }
+
+#[inline(always)]
+pub fn stoi(s: &str) -> usize {
+    let mut i = 0;
+    s.as_bytes()
+        .iter()
+        .for_each(|c| i = (10 * i) + (c - b'0') as usize);
+    i
+}
