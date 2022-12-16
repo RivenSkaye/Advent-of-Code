a0c2022732815c465b1db65c5217614b39368599
Riven Skaye <riven@tae.moe>
2022-12-16 16:16:46 +0100

    Remove memetier unsafe, it's not even benched

--- 2022/src/common.rs
+++ 2022/src/common.rs
@@ -6,7 +6,7 @@ const FILENAME: &str = "inputs";
 #[inline(always)]
 pub fn read_file<const DAY: usize>() -> String {
     println!("Reading `{FILENAME}`");
-    unsafe { std::fs::read_to_string(format!("{FILENAME}/{DAY:0>2}.txt")).unwrap_unchecked() }
+    std::fs::read_to_string(format!("{FILENAME}/{DAY:0>2}.txt")).unwrap()
 }
 
 #[inline(always)]
