a0c20220f892ce01b1f53d80856fb063d58a36d0
Riven Skaye <riven@tae.moe>
2022-12-19 14:01:58 +0100

    Very small gains

--- 2022/src/bin/day15.rs
+++ 2022/src/bin/day15.rs
@@ -95,26 +95,28 @@ const MAXRANGE: i64 = 4000001;
 const MAXRANGE: i64 = 21;
 
 #[inline(always)]
-pub fn in_range(point: Point, sensor: &Sensor) -> bool {
-    (point.0.abs_diff(sensor.point.0) + point.1.abs_diff(sensor.point.1)) < sensor.range + 1
+pub fn in_range(x: i64, y: i64, sensor: &Sensor) -> bool {
+    (x.abs_diff(sensor.point.0) + y.abs_diff(sensor.point.1)) < sensor.range + 1
 }
 
 pub fn part_two(parsed: Vec<Sensor>) -> i64 {
     let mut x = MAXRANGE - 1;
     let mut y = 0;
-    'xloop: loop {
-        while y < MAXRANGE {
-            if let Some(sensor) = &parsed.iter().find(|sensor| in_range(Point(x, y), sensor)) {
-                y = ((sensor.point.1 + sensor.range as i64) - sensor.point.0.abs_diff(x) as i64)
-                    + 1;
-            } else {
-                break 'xloop;
+    loop {
+        'yloop: while y < MAXRANGE {
+            for sensor in &parsed {
+                if in_range(x, y, sensor) {
+                    y = ((sensor.point.1 + sensor.range as i64)
+                        - sensor.point.0.abs_diff(x) as i64)
+                        + 1;
+                    continue 'yloop;
+                }
             }
+            return (x * 4000000) + y;
         }
         y = 0;
         x -= 1;
     }
-    (x * 4000000) + y
 }
 
 pub fn main() {
