a0c2022beeeae91ce57d96fe114646a30e5bdf4f
Riven Skaye <riven@tae.moe>
2022-12-19 13:47:54 +0100

    A little bit of rewriting, SPEED

--- 2022/src/bin/day15.rs
+++ 2022/src/bin/day15.rs
@@ -1,19 +1,21 @@
 #![feature(test)]
-use std::{collections::HashSet, ops::Sub};
 
-use aoc2022::common::{read_file, stosi};
+use aoc2022::common::read_file;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub struct Point(isize, isize);
-impl From<(isize, isize)> for Point {
-    fn from((x, y): (isize, isize)) -> Self {
-        Self(x, y)
+#[inline(always)]
+pub fn stosi(s: &[u8]) -> i64 {
+    match s[0] == b'-' {
+        true => s[1..].iter().fold(0, |i, c| (10 * i) - (c - b'0') as i64),
+        false => s.iter().fold(0, |i, c| (10 * i) + (c - b'0') as i64),
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Point(i64, i64);
+
+#[derive(Copy, Clone)]
 pub struct Sensor {
-    range: usize,
+    range: u64,
     point: Point,
     beaconpos: Point,
 }
@@ -28,65 +30,46 @@ impl Sensor {
     }
 }
 
+#[inline]
 pub fn parse(input: &str) -> Vec<Sensor> {
     // Strip off all the useless garbo, it's noise.
     // That leaves us with xxx,yyy:xxx,yyy points!
     input
-        .replace("Sensor at x=", "")
-        .replace(" closest beacon is at x=", "")
-        .replace(" y=", "")
         .lines()
         .map(|line| {
-            let mut sensor = (0, 0);
-            let mut beacon = (0, 0);
-            let mut counter = 0;
-            line.split(':').for_each(|chunk| {
-                chunk.split(',').for_each(|n| {
-                    match counter % 4 {
-                        0 => sensor.0 = stosi(n),
-                        1 => sensor.1 = stosi(n),
-                        2 => beacon.0 = stosi(n),
-                        3 => beacon.1 = stosi(n),
-                        _ => unreachable!(),
-                    }
-                    counter += 1;
-                });
-            });
-            Sensor::new(sensor.into(), beacon.into())
+            let mut l = line.split(':');
+            Sensor::new(
+                l.next()
+                    .map(|chunk| {
+                        chunk[12..]
+                            .split_once(',')
+                            .map(|(s0, s1)| Point(stosi(s0.as_bytes()), stosi(s1[3..].as_bytes())))
+                    })
+                    .unwrap()
+                    .unwrap(),
+                l.next()
+                    .map(|chunk| {
+                        chunk[24..]
+                            .split_once(',')
+                            .map(|(b0, b1)| Point(stosi(b0.as_bytes()), stosi(b1[3..].as_bytes())))
+                    })
+                    .unwrap()
+                    .unwrap(),
+            )
         })
         .collect()
 }
 #[cfg(not(debug_assertions))]
-const TARGET: isize = 2000000;
+const TARGET: i64 = 2000000;
 #[cfg(debug_assertions)]
-const TARGET: isize = 10;
+const TARGET: i64 = 10;
 
-#[cfg(not(debug_assertions))]
-const MAXRANGE: i64 = 4000001;
-#[cfg(debug_assertions)]
-const MAXRANGE: i64 = 20;
-
-pub fn part_one(mut parsed: Vec<Sensor>) -> i64 {
-    parsed.sort_unstable_by(|l, r| {
-        let ldiff = l.point.1.abs_diff(TARGET);
-        let rdiff = r.point.1.abs_diff(TARGET);
-        if ldiff > l.range && rdiff > r.range {
-            std::cmp::Ordering::Equal
-        } else if ldiff > l.range {
-            std::cmp::Ordering::Greater
-        } else if rdiff > r.range {
-            std::cmp::Ordering::Less
-        } else {
-            (l.range - ldiff).cmp(&(r.range - rdiff))
-        }
-    });
-    // We know we'll cover a maximum amount of ranges equal to the amount of sensors
-    // though most cases where a sensor doesn't cover the target line, we won't
-    // need to consider it, so we'll be using less than allocated. Single alloc gang!
-    let mut covered = Vec::<(isize, isize)>::with_capacity(parsed.len());
+#[inline]
+pub fn part_one(parsed: Vec<Sensor>) -> i64 {
+    let mut covered = (0, 0);
     // We'll need to keep track of all beacons on the target line, as they're valid
     // beacon positions and need to be subtracted from the amount of scanned positions.
-    let mut beacons = HashSet::with_capacity(parsed.len());
+    let mut beacons = Vec::with_capacity(5);
     parsed.iter().for_each(|sensor| {
         // The scanning width will decrease by 2 for every Y point difference,
         // the maximum width is (range * 2) + 1, as it scans its Manhattan distance
@@ -94,67 +77,22 @@ pub fn part_one(mut parsed: Vec<Sensor>) -> i64 {
         // for its entire range.
         let dist = sensor.point.1.abs_diff(TARGET);
         if dist < sensor.range + 1 {
-            if sensor.beaconpos.1 == TARGET {
-                beacons.insert(sensor.beaconpos);
+            if sensor.beaconpos.1 == TARGET && !beacons.contains(&sensor.beaconpos) {
+                beacons.push(sensor.beaconpos)
             }
-            let min = sensor.point.0 - (sensor.range - dist) as isize;
-            let max = sensor.point.0 + (sensor.range - dist) as isize;
-            let mut push = true;
-            for i in 0..covered.len() {
-                // If this max lies within any covered range and the min is lower,
-                // just expand the range as we have overlap
-                if covered[i].0 > min - 1 && covered[i].1 > max - 1 && covered[i].0 < max + 1 {
-                    covered[i].0 = min;
-                    push = false;
-                    break;
-                }
-                // Same check for the min inside and the max outside
-                if covered[i].1 < max + 1 && covered[i].0 < min + 1 && covered[i].1 > min - 1 {
-                    covered[i].1 = max;
-                    push = false;
-                    break;
-                }
-                // If this range contains an existing range entirely, grow the existing range
-                if covered[i].0 > min - 1 && covered[i].1 < max + 1 {
-                    covered[i] = (min, max);
-                    push = false;
-                    break;
-                }
-                // Ignore the range if it's entirely contained in another
-                if covered[i].0 < min + 1 && covered[i].1 > max - 1 {
-                    push = false;
-                }
-            }
-            if push {
-                covered.push((min, max));
-            }
+            let min = sensor.point.0 - (sensor.range - dist) as i64;
+            let max = sensor.point.0 + (sensor.range - dist) as i64;
+            covered.0 = min.min(covered.0);
+            covered.1 = max.max(covered.1);
         }
     });
-    for i in 0..covered.len() {
-        for j in 0..covered.len() {
-            if i == j {
-                continue;
-            }
-            // If we find overlap
-            if covered[i].1 >= covered[j].0 && covered[i].0 <= covered[j].0 {
-                // truncate the range with the higher starting number
-                covered[j].0 = covered[i].1 + 1;
-            }
-        }
-    }
-    covered
-        .iter()
-        .map(|covrange| {
-            if covrange.0 > covrange.1 {
-                0
-            } else {
-                // 3.abs_diff(5) = 2. But if a range contains 3..=5, it's 3 positions
-                (covrange.0.abs_diff(covrange.1) + 1) as i64
-            }
-        })
-        .sum::<i64>()
-        .sub(beacons.len() as i64)
+    ((covered.0.abs_diff(covered.1)) - beacons.len() as u64) as i64 + 1
 }
+
+#[cfg(not(debug_assertions))]
+const MAXRANGE: i64 = 4000001;
+#[cfg(debug_assertions)]
+const MAXRANGE: i64 = 21;
 
 #[inline(always)]
 pub fn in_range(point: Point, sensor: &Sensor) -> bool {
@@ -163,23 +101,20 @@ pub fn in_range(point: Point, sensor: &Sensor) -> bool {
 
 pub fn part_two(parsed: Vec<Sensor>) -> i64 {
     let mut x = MAXRANGE - 1;
-    loop {
-        let mut y: i64 = 0;
+    let mut y = 0;
+    'xloop: loop {
         while y < MAXRANGE {
-            match &parsed
-                .iter()
-                .find(|sensor| in_range(Point(x as isize, y as isize), sensor))
-            {
-                Some(sensor) => {
-                    y = ((sensor.point.1 as usize + sensor.range)
-                        - sensor.point.0.abs_diff(x as isize)) as i64
-                        + 1;
-                }
-                _ => return (x * 4000000) + y,
+            if let Some(sensor) = &parsed.iter().find(|sensor| in_range(Point(x, y), sensor)) {
+                y = ((sensor.point.1 + sensor.range as i64) - sensor.point.0.abs_diff(x) as i64)
+                    + 1;
+            } else {
+                break 'xloop;
             }
         }
+        y = 0;
         x -= 1;
     }
+    (x * 4000000) + y
 }
 
 pub fn main() {
