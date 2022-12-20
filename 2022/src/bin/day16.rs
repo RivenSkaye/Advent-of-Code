a0c2022749379419b1a289eb74f557d9ce7446ed
Riven Skaye <riven@tae.moe>
2022-12-20 08:50:04 +0100

    This do be slightly cleaner

--- 2022/src/bin/day16.rs
+++ 2022/src/bin/day16.rs
@@ -11,6 +11,7 @@ pub fn parse(input: &str) -> HashMap<u16, Valve> {
     input
         .lines()
         .map(|line| {
+            let mut routes = 50;
             (
                 line[6..8]
                     .as_bytes()
@@ -18,17 +19,15 @@ pub fn parse(input: &str) -> HashMap<u16, Valve> {
                     .fold(0, |a, b| (a << 8) | *b as u16),
                 Valve {
                     rate: line[23..25].as_bytes().iter().fold(0, |a, b| match b {
-                        b';' => a,
+                        b';' => {
+                            routes = 49;
+                            a
+                        }
                         _ => (10 * a) + (b - b'0') as i64,
                     }),
-                    connections: line[49..]
+                    connections: line[routes..]
                         .split(", ")
-                        .map(|valve| {
-                            valve.as_bytes().iter().fold(0, |a, b| match b {
-                                b' ' => a,
-                                _ => (a << 8) | *b as u16,
-                            })
-                        })
+                        .map(|valve| valve.as_bytes().iter().fold(0, |a, b| (a << 8) | *b as u16))
                         .collect(),
                     open: false,
                 },
