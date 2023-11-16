013086258f946cc9f3c7393d56a48face4f7310d
Riven Skaye <riven@tae.moe>
2023-11-16 14:51:58 +0100

    Previous years are still chances to improve myself

---
+++ 2015/src/common.rs
@@ -0,0 +1,25 @@
+#[cfg(debug_assertions)]
+const FILENAME: &str = "test_inputs";
+#[cfg(not(debug_assertions))]
+const FILENAME: &str = "inputs";
+
+#[inline(always)]
+pub fn read_file<const DAY: usize>() -> String {
+    println!("Reading `{FILENAME}`");
+    std::fs::read_to_string(format!("{FILENAME}/{DAY:0>2}.txt")).unwrap()
+}
+
+#[inline(always)]
+pub fn stoi(s: &str) -> usize {
+    s.as_bytes()
+        .iter()
+        .fold(0, |i, c| (10 * i) + (c - b'0') as usize)
+}
+
+#[inline(always)]
+pub fn stosi(s: &[u8]) -> i64 {
+    match s[0] == b'-' {
+        true => s[1..].iter().fold(0, |i, c| (10 * i) - (c - b'0') as i64),
+        false => s.iter().fold(0, |i, c| (10 * i) + (c - b'0') as i64),
+    }
+}
