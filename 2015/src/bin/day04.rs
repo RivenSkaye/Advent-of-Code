5c86accc42412ac0d307c94dd377bfbb19b1a6e0
Riven Skaye <riven@tae.moe>
2023-11-27 16:32:35 +0100

    I spent like 5 minutes in my break

---
+++ 2015/src/bin/day04.rs
@@ -0,0 +1,30 @@
+use aoc2015::common;
+use md5::{Md5, Digest};
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+const INPUT: &'static str = "yzbqklnj";
+
+fn part_one() -> usize {
+    for i in 0..=usize::MAX {
+        let dig = format!("{:x}", Md5::digest(format!("{INPUT}{i}")));
+        if dig.starts_with("00000") { return i }
+    }
+    unreachable!()
+}
+
+fn part_two(start: usize) -> usize {
+    for i in start..=usize::MAX {
+        let dig = format!("{:x}", Md5::digest(format!("{INPUT}{i}")));
+        if dig.starts_with("000000") { return i }
+    }
+    unreachable!()
+}
+
+fn main() {
+    let p1 = part_one();
+    println!("{}", p1);
+    println!("{}", part_two(p1))
+}
