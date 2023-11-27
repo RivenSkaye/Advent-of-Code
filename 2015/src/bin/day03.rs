4a1dae36ce536fcb894c536aed4410ecab956f20
Riven Skaye <riven@tae.moe>
2023-11-27 11:06:51 +0100

    fast code go fast

--- 2015/src/bin/day03.rs
+++ 2015/src/bin/day03.rs
@@ -1,37 +1,61 @@
 #![feature(test)]
 extern crate test;
 
+use std::collections::HashSet;
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 use aoc2015::common;
 
-struct Point {
-    x: isize,
-    y: isize,
-}
+#[derive(Debug, Eq, PartialEq, Clone, Copy, Hash)]
+struct Point(isize, isize);
 
-impl From<(isize, isize)> for Point {
-    fn from(value: (isize, isize)) -> Self {
-        Self {
-            x: value.0,
-            y: value.1,
-        }
+impl Point {
+    fn step(&mut self, s: &u8) -> &Self {
+        match s {
+            b'^' => self.1 += 1,
+            b'v' => self.1 -= 1,
+            b'>' => self.0 += 1,
+            b'<' => self.0 -= 1,
+            _ => unreachable!()
+        };
+        self
     }
 }
 
-impl From<(Point, u8)> for Point {
-    fn from(value: (Point, u8)) -> Self {
-        match value.1 {
-            b'<' => (value.0.x - 1, value.0.y),
-            b'^' => (value.0.x, value.0.y + 1),
-            b'>' => (value.0.x + 1, value.0.y),
-            b'v' => (value.0.x, value.0.y + 1),
-            _ => unreachable!(),
-        }
-        .into()
+const START: Point = Point(0,0);
+
+fn part_one(input: &[u8]) -> usize {
+    let mut last = START;
+    let mut points = HashSet::with_capacity(5200);
+    points.insert(last);
+    input.iter().for_each(|step| {
+        points.insert(*last.step(step));
+    });
+    points.len()
+}
+
+fn main() {
+    let bs = common::read_file::<3>().as_bytes().to_owned();
+    println!("{}", part_one(&bs))
+}
+
+#[cfg(test)]
+mod aoc_benching {
+    use super::*;
+
+    #[bench]
+    fn p1bench(b: &mut test::Bencher) {
+        let bs = common::read_file::<3>().as_bytes().to_owned();
+        b.iter(|| assert_eq!(part_one(test::black_box(&bs)), 2572));
     }
+/*
+    #[bench]
+    fn p2bench(b: &mut test::Bencher) {
+        let input = common::read_file::<2>();
+        let bs = parse(input.as_bytes());
+        b.iter(|| assert_eq!(part_two(test::black_box(&bs)), 3842356));
+    }*/
 }
-
-fn part_one(input: &[u8]) -> isize {
-    0
-}
-
-fn main() {}
