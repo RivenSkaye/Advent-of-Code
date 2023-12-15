70e19bace606b037563ced3ee0b9d1a53443e033
Riven Skaye <riven@tae.moe>
2023-12-15 16:16:44 +0100

    d10p1 done, did some shifts and prep for p2

--- 2023/src/bin/day10.rs
+++ 2023/src/bin/day10.rs
@@ -5,7 +5,9 @@ use mimalloc::MiMalloc;
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-use std::mem::transmute;
+use std::{mem::transmute, ops::Div};
+
+use aoc2023::common;
 
 /// Safely transmute valid map entries (u8) to Pipes, see
 /// [faster than lime](https://fasterthanli.me/articles/peeking-inside-a-rust-enum)
@@ -23,34 +25,166 @@ pub enum Pipe {
     Start = b'S',     // Wildcard
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum Direction {
+    Left = 1,
+    Right = 2,
+    Up = 4,
+    Down = 8,
+}
+
 #[cfg(debug_assertions)]
 const MAP_DIMENSIONS: usize = 5;
 #[cfg(not(debug_assertions))]
 const MAP_DIMENSIONS: usize = 140;
 
-pub struct Point {
-    x: usize,
-    y: usize,
-}
+const ALL_DIRS: [Direction; 4] = [
+    Direction::Left,
+    Direction::Up,
+    Direction::Right,
+    Direction::Down,
+];
+
+#[derive(Debug, Clone, Copy)]
+pub struct Point(usize, usize);
 
 pub type Map = [[Pipe; MAP_DIMENSIONS]; MAP_DIMENSIONS];
 
-pub fn parse(input: &[u8]) -> (Map, (usize, usize)) {
-    let mut map: Map = [[0; MAP_DIMENSIONS]; MAP_DIMENSIONS];
-    let mut start = (0, 0);
+pub fn parse(input: &[u8]) -> (Map, Point) {
+    let mut map: Map = [[Pipe::NoPipe; MAP_DIMENSIONS]; MAP_DIMENSIONS];
+    let mut start = Point(0, 0);
     input
         .split(|c| b'\n'.eq(c))
         .enumerate()
         .for_each(|(y, line)| {
-            line.iter().enumerate().for_each(|(x, chr)| {
-                if b'S'.eq(chr) {
-                    start = (y, x);
+            line.iter().enumerate().for_each(|(x, &chr)| {
+                if b'S' == chr {
+                    start.0 = y;
+                    start.1 = x;
                 }
                 // SAFETY: see Pipe
-                map[y][x] = unsafe { transmute(*chr) };
+                map[y][x] = unsafe { transmute(chr) };
             })
         });
     (map, start)
 }
 
-pub fn main() {}
+impl Direction {
+    #[inline(always)]
+    fn check_step(target: Pipe) -> Option<Pipe> {
+        (target != Pipe::NoPipe).then_some(target)
+    }
+
+    #[inline(always)]
+    fn get_next(self, next: Pipe) -> Option<Self> {
+        match (&self, next) {
+            // Moving horizontally, this pipe lets us continue straight ahead
+            (Self::Left | Self::Right, Pipe::LeftRight) => Some(self),
+            // Moving left, this pipe bends up
+            (Self::Left, Pipe::DownRight) => Some(Self::Up),
+            // Moving left, this pipe bends down
+            (Self::Left, Pipe::UpRight) => Some(Self::Down),
+            // Moving right, this bends up
+            (Self::Right, Pipe::DownLeft) => Some(Self::Up),
+            // Moving to the right, this bends down
+            (Self::Right, Pipe::UpLeft) => Some(Self::Down),
+            // Moving vertically, this pipe lets us continue
+            (Self::Up | Self::Down, Pipe::UpDown) => Some(self),
+            // Moving up, this bends us left
+            (Self::Up, Pipe::UpLeft) => Some(Self::Left),
+            // Moving up, this bends us right
+            (Self::Up, Pipe::UpRight) => Some(Self::Right),
+            // Moving down, left turn
+            (Self::Down, Pipe::DownLeft) => Some(Self::Left),
+            // moving down, right turn
+            (Self::Down, Pipe::DownRight) => Some(Self::Right),
+            // If you're feeling down, I can feel you up
+            _ => None,
+        }
+    }
+
+    pub fn next_coords(self, pos: &Point) -> Option<Point> {
+        match self {
+            Self::Left if pos.1 != 0 => Some(Point(pos.0, pos.1 - 1)),
+            Self::Right if pos.1 != usize::MAX => Some(Point(pos.0, pos.1 + 1)),
+            Self::Up if pos.0 != 0 => Some(Point(pos.0 - 1, pos.1)),
+            Self::Down if pos.0 != usize::MAX => Some(Point(pos.0 + 1, pos.1)),
+            _ => None,
+        }
+    }
+
+    pub fn step(self, pos: &Point, map: &Map) -> Option<(Self, Point)> {
+        let next_pos = self.next_coords(pos)?;
+        // Check step returns None if the next pipe in this direction is a wall
+        // get_next returns None if the next pipe is valid but can't be entered
+        // If a direction comes back, provide the next point to check
+        Self::check_step(map[next_pos.0][next_pos.1])
+            .and_then(|pipe| self.get_next(pipe).map(|dir| (dir, next_pos)))
+    }
+}
+
+pub fn part_one((map, start_pos): &(Map, Point)) -> usize {
+    ALL_DIRS
+        .iter()
+        // Find the first entrance to the loop; there should only be 2 openings
+        .find_map(|&direction| {
+            let mut pos = start_pos.to_owned();
+            let mut dir = direction;
+            let mut steps = 0;
+            while let Some((d, p)) = dir.step(&pos, &map) {
+                pos = p;
+                dir = d;
+                steps += 1;
+                if map[pos.0][pos.1] == Pipe::Start {
+                    return Some(steps / 2);
+                }
+            }
+            match dir.next_coords(&pos)? {
+                Point(y, x) if map[y][x] == Pipe::Start => Some(steps + 1),
+                _ => None,
+            }
+        })
+        .unwrap()
+        .div(2usize)
+}
+
+pub fn part_two((map, start_pos): &(Map, Point)) -> usize {
+    // Cheap optimization: skip checking any rows and columns outside of the loop's outer bounds
+    let mut min_y = usize::MAX;
+    let mut max_y = usize::MIN;
+    let mut min_x = usize::MAX;
+    let mut max_x = usize::MIN;
+    0
+}
+
+pub fn main() {
+    let data = common::read_file::<10>();
+    let parsed = parse(&data);
+    println!("{}", part_one(&parsed));
+    //println!("{}", part_two(&parsed));
+}
+
+#[cfg(test)]
+mod aoc_benching {
+    use super::*;
+
+    #[bench]
+    fn parsebench(b: &mut test::Bencher) {
+        let input = common::read_file::<10>();
+        b.iter(|| parse(test::black_box(&input)))
+    }
+
+    #[bench]
+    fn part1bench(b: &mut test::Bencher) {
+        let input = common::read_file::<10>();
+        let parsed = parse(&input);
+        b.iter(|| assert_eq!(part_one(test::black_box(&parsed)), 6842))
+    }
+
+    /*#[bench]
+    fn part2bench(b: &mut test::Bencher) {
+        let input = common::read_file::<10>();
+        let parsed = parse(&input);
+        b.iter(|| assert_eq!(part_two(test::black_box(&parsed)), 928))
+    }*/
+}
