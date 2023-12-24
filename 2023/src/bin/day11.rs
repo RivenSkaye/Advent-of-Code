a0c2023c301ff9bddf4f98e852eba1d6f2d7f3a8
Riven Skaye <riven@tae.moe>
2023-12-25 00:52:46 +0100

    Less math, more speed

--- 2023/src/bin/day11.rs
+++ 2023/src/bin/day11.rs
@@ -9,17 +9,21 @@ static GLOBAL: MiMalloc = MiMalloc;
 
 use aoc2023::common;
 
-// Literally just for the benches
+/// Points for today store _two_ pieces of info per axis.
+/// 0: Vertical (y) coordinate
+/// 1: Horizontal (x) coordinate
+/// 2: The amount of lines to expand for y
+/// 3: The amount of lines to expand for x
 #[derive(Debug, PartialEq)]
 pub struct Point(usize, usize, usize, usize);
 
 impl Point {
     /// This is all about manhattan distances
     pub fn distance<const EXPANSION: usize>(&self, other: &Self) -> usize {
-        let sx = self.1 + (self.3 * EXPANSION) - self.3;
-        let sy = self.0 + (self.2 * EXPANSION) - self.2;
-        let ox = other.1 + (other.3 * EXPANSION) - other.3;
-        let oy = other.0 + (other.2 * EXPANSION) - other.2;
+        let sx = self.1 + (self.3 * EXPANSION);
+        let sy = self.0 + (self.2 * EXPANSION);
+        let ox = other.1 + (other.3 * EXPANSION);
+        let oy = other.0 + (other.2 * EXPANSION);
         sx.abs_diff(ox) + sy.abs_diff(oy)
     }
 }
@@ -80,7 +84,7 @@ pub fn part_one(points: &[Point]) -> usize {
         .iter()
         .zip(1..)
         .fold(0, |last, (point, next)| {
-            last + (next..len).fold(0, |l, i| l + points[i].distance::<2>(point))
+            last + (next..len).fold(0, |l, i| l + points[i].distance::<1>(point))
         })
 }
 
@@ -90,7 +94,7 @@ pub fn part_two(points: &[Point]) -> usize {
         .iter()
         .zip(1..)
         .fold(0, |last, (point, next)| {
-            last + (next..len).fold(0, |l, i| l + points[i].distance::<1000000>(point))
+            last + (next..len).fold(0, |l, i| l + points[i].distance::<999999>(point))
         })
 }
 
