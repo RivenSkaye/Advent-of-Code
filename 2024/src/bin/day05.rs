f82f96c49d8e5eac70680690c7b1362443ccc2ae
Riven Skaye <riven@tae.moe>
2024-12-05 09:38:33 +0100

    Is it slow as shit? Yes. Could it be much better? Also yes. But it works, so we can move on

--- 2024/src/bin/day05.rs
+++ 2024/src/bin/day05.rs
@@ -1,7 +1,7 @@
-#![feature(array_windows, test)]
+#![feature(test)]
 extern crate test;
 
-use std::{collections::HashMap, io::BufRead};
+use std::{collections::HashSet, io::BufRead};
 
 use mimalloc::MiMalloc;
 #[global_allocator]
@@ -9,7 +9,7 @@ static GLOBAL: MiMalloc = MiMalloc;
 
 use aoc2024::common;
 
-pub fn parse(input: &[u8]) -> (HashMap<usize, usize>, Vec<Vec<usize>>) {
+pub fn parse(input: &[u8]) -> (HashSet<(usize, usize)>, Vec<Vec<usize>>) {
     let mut line_iter = input.lines();
     let failures = line_iter
         .by_ref()
@@ -17,10 +17,9 @@ pub fn parse(input: &[u8]) -> (HashMap<usize, usize>, Vec<Vec<usize>>) {
             if let Ok(l) = line {
                 (!l.is_empty()).then(|| {
                     let (first, last) = l.split_once('|').unwrap();
-                    println!("{first}|{last}");
                     (
-                        common::stoi(last.as_bytes()),
                         common::stoi(first.as_bytes()),
+                        common::stoi(last.as_bytes()),
                     )
                 })
             } else {
@@ -39,33 +38,101 @@ pub fn parse(input: &[u8]) -> (HashMap<usize, usize>, Vec<Vec<usize>>) {
     (failures, prints)
 }
 
-pub fn part_one(failures: &HashMap<usize, usize>, prints: &[Vec<usize>]) -> usize {
+#[inline]
+fn check_job(failures: &HashSet<(usize, usize)>, job: &[usize]) -> Result<usize, (usize, usize)> {
+    let mut printed = Vec::with_capacity(job.len());
+    let middle = job.len() / 2; // integer division truncates
+    for page in job {
+        if let Some(wrong) = failures
+            .iter()
+            .find(|(f, p)| job.contains(f) && !printed.contains(f) && p == page)
+        {
+            return Err(*wrong);
+        } else {
+            printed.push(*page);
+        }
+    }
+    // we know this exists, this should not generate a bounds check
+    Ok(job[middle])
+}
+
+pub fn part_one(failures: &HashSet<(usize, usize)>, prints: &[Vec<usize>]) -> usize {
     prints
         .iter()
+        .filter_map(|job| check_job(failures, job).ok())
+        .sum()
+}
+
+pub fn part_two(failures: &HashSet<(usize, usize)>, prints: &mut [Vec<usize>]) -> usize {
+    prints
+        .iter_mut()
         .filter_map(|job| {
-            println!("{job:?}");
-            let mut printed = Vec::with_capacity(job.len());
-            let middle = (job.len() / 2) + 1; // integer division truncates
-            for page in job {
-                if let Some(fnum) = failures.get(page) {
-                    if !printed.contains(fnum) && job.contains(fnum) {
-                        println!("{page} was out of order");
-                        return None;
-                    }
-                    printed.push(*page);
-                } else {
-                    printed.push(*page);
-                }
+            let mut res = None;
+            while let Some(off) = check_job(failures, job).err() {
+                let a = job.iter().position(|e| off.0.eq(e)).unwrap();
+                let b = job.iter().position(|e| off.1.eq(e)).unwrap();
+                job.swap(a, b);
+                res = Some(job[job.len() / 2]);
             }
-            println!("{} is A-OK", job[middle]);
-            // we know this exists, this should not generate a bounds check
-            Some(printed[middle])
+            res
         })
         .sum()
 }
 
 pub fn main() {
     let input = common::read_file::<5>();
-    let (fails, prints) = parse(&input);
+    let (fails, mut prints) = parse(&input);
     println!("{}", part_one(&fails, &prints));
+    println!("{}", part_two(&fails, &mut prints));
+}
+
+#[cfg(test)]
+mod aoc_benching {
+    use super::*;
+
+    #[bench]
+    fn parsebench(b: &mut test::Bencher) {
+        let input = common::read_file::<5>();
+        let parsed = parse(&input);
+        b.iter(|| assert_eq!(parse(test::black_box(&input)), parsed))
+    }
+
+    #[bench]
+    fn part1bench(b: &mut test::Bencher) {
+        let input = common::read_file::<5>();
+        let parsed = parse(&input);
+        b.iter(|| {
+            assert_eq!(
+                part_one(
+                    test::black_box(&parsed.0),
+                    test::black_box(&parsed.1.clone())
+                ),
+                5108
+            )
+        })
+    }
+
+    #[bench]
+    fn part2bench(b: &mut test::Bencher) {
+        let input = common::read_file::<5>();
+        let parsed = parse(&input);
+        b.iter(|| {
+            assert_eq!(
+                part_two(
+                    test::black_box(&parsed.0),
+                    test::black_box(&mut parsed.1.clone())
+                ),
+                7380
+            )
+        })
+    }
+
+    #[bench]
+    fn part2clone_diff(b: &mut test::Bencher) {
+        let input = common::read_file::<5>();
+        let parsed = parse(&input);
+        b.iter(|| {
+            assert_eq!(test::black_box(&mut parsed.1.clone()), &parsed.1);
+        })
+    }
 }
