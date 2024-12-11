a0c2024f7b070ceffec9b7c02096122c9ae651e9
Riven Skaye <riven@tae.moe>
2024-12-11 21:10:05 +0100

    Finally got time, mid-progress push for when I inevitably need to go back. Result still too high

--- 2024/src/bin/day10.rs
+++ 2024/src/bin/day10.rs
@@ -3,6 +3,8 @@ extern crate test;
 
 use std::usize;
 
+use aoc2024::common;
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 #[repr(u8)]
 pub enum Direction {
@@ -10,17 +12,6 @@ pub enum Direction {
     DOWN,
     LEFT,
     RIGHT,
-}
-
-impl Direction {
-    fn rev(&self) -> Self {
-        match self {
-            Direction::UP => Direction::DOWN,
-            Direction::DOWN => Direction::UP,
-            Direction::LEFT => Direction::RIGHT,
-            Direction::RIGHT => Direction::LEFT,
-        }
-    }
 }
 
 #[derive(Clone)]
@@ -41,49 +32,52 @@ impl Grid {
         }?;
         let nval = self.inner[nextpos];
         let fval = self.inner[from];
-        if fval.abs_diff(nval) > 1 || fval > nval {
-            None
-        } else {
-            nextpos.into()
-        }
+        (fval.abs_diff(nval) == 1 && fval < nval).then_some(nextpos)
     }
 }
 
-pub fn parse(input: &[u8]) -> (Grid, Vec<usize>) {
-    let len = input.iter().position(|c| b'\n'.eq(c)).unwrap();
-    //over-allocate and make the assumption 1 in 5 numbers is a zero. This cound also still includes newlines.
-    let mut tailheads = Vec::with_capacity(input.len() / 5);
-    (
-        Grid {
-            line_length: len,
-            inner: input
-                .iter()
-                .filter(|c| b'\n'.ne(c))
-                .enumerate()
-                .map(|(idx, p)| {
-                    if b'0'.eq(p) {
-                        tailheads.push_within_capacity(idx).unwrap();
-                    }
-                    *p
-                })
-                .collect(),
-        },
-        tailheads,
-    )
+pub fn parse(input: &[u8]) -> Grid {
+    Grid {
+        line_length: input.iter().position(|c| b'\n'.eq(c)).unwrap(),
+        inner: input
+            .iter()
+            .filter_map(|c| b'\n'.ne(c).then(|| *c))
+            .collect(),
+    }
 }
 
-fn walk_from(grid: Grid, position: usize, start_dir: Direction) -> usize {
+fn walk_from(grid: &Grid, position: usize, ends: &mut Vec<usize>) {
     const DIRECTIONS: [Direction; 4] = [
         Direction::UP,
         Direction::DOWN,
         Direction::LEFT,
         Direction::RIGHT,
     ];
-    DIRECTIONS.iter().filter(|&&d| d != start_dir.rev()).map({
-        let next;
-    })
+    if grid.inner[position] == b'9' {
+        return ends.push_within_capacity(position).unwrap();
+    }
+    DIRECTIONS
+        .iter()
+        .filter_map(|&dir| grid.step(position, dir))
+        .for_each(|position| walk_from(grid, position, ends));
 }
 
-pub fn part_one(grid: Grid, heads: &[u8]) -> usize {}
+pub fn part_one(grid: &Grid) -> usize {
+    grid.inner
+        .iter()
+        .enumerate()
+        .filter_map(|(idx, val)| {
+            b'0'.eq(val).then(|| {
+                let mut ends = Vec::with_capacity(grid.inner.len());
+                walk_from(grid, idx, &mut ends);
+                ends.len()
+            })
+        })
+        .sum()
+}
 
-pub fn main() {}
+pub fn main() {
+    let input = common::read_file::<10>();
+    let parsed = parse(&input);
+    println!("{}", part_one(&parsed));
+}
