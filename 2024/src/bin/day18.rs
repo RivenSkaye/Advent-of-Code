a0c2024b7495f59082e098e118941a5830eaf3af
Riven Skaye <riven@tae.moe>
2024-12-22 23:31:08 +0100

    `import solution` because fuck this

--- 2024/src/bin/day18.rs
+++ 2024/src/bin/day18.rs
@@ -1,114 +1,120 @@
 #![feature(slice_split_once, test)]
 extern crate test;
 
-use std::collections::HashMap;
+use pathfinding::prelude::dijkstra;
 
-use aoc2024::{
-    common,
-    grids::{Direction, FlatGrid as Grid},
-};
+use aoc2024::common;
 
 #[cfg(debug_assertions)]
-const WIDTH: usize = 7; // 0 through 6
+const WIDTH: usize = 6;
 #[cfg(not(debug_assertions))]
-const WIDTH: usize = 71; // 0 through 70
+const WIDTH: usize = 70;
 
 #[cfg(debug_assertions)]
-const HEIGHT: usize = 7; // 0 through 6
+const HEIGHT: usize = 6;
 #[cfg(not(debug_assertions))]
-const HEIGHT: usize = 71; // 0 through 70
+const HEIGHT: usize = 70;
 
 #[cfg(debug_assertions)]
 const TTL: usize = 12;
 #[cfg(not(debug_assertions))]
 const TTL: usize = 1024;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Position {
     x: usize,
     y: usize,
 }
 
+const GOAL: Position = Position {
+    x: WIDTH,
+    y: HEIGHT,
+};
+
 impl Position {
-    pub fn to_flat_pos(&self) -> usize {
-        (self.y * WIDTH) + self.x
+    fn left(&self) -> Option<Self> {
+        self.x.checked_sub(1).map(|x| Self { x, y: self.y })
     }
-}
-
-pub fn parse(data: &[u8]) -> (Grid, Vec<Position>) {
-    (
-        Grid {
-            inner: [b'.'; WIDTH * HEIGHT].into(),
-            line_length: WIDTH,
-        },
-        data.split(|chr| b'\n'.eq(chr))
-            .map(|line| {
-                line.split_once(|chr| b','.eq(chr))
-                    .map(|(x, y)| Position {
-                        x: common::stoi(x),
-                        y: common::stoi(y),
-                    })
-                    .unwrap()
-            })
-            .collect(),
-    )
-}
-
-fn walk(
-    grid: &mut Grid,
-    current: usize,
-    dir: Direction,
-    visited: &mut HashMap<(usize, Direction), usize>,
-    steps: usize,
-    max_steps: &mut usize,
-) -> Option<usize> {
-    if current == grid.inner.len() - 1 {
-        if steps.lt(max_steps) {
-            *max_steps = steps;
-        }
-        return Some(steps);
-    }
-    visited.insert((current, dir), steps);
-
-    [dir.turn_clock(), dir, dir.turn_counter()]
-        .iter()
-        .filter_map(|&dir| {
-            if let Some(next) = grid.bounded_step(current, dir) {
-                if grid.inner[next] == b'#'
-                    || steps.ge(max_steps)
-                    || visited.get(&(next, dir)).is_some_and(|s| steps.ge(s))
-                {
-                    None
-                } else {
-                    walk(grid, next, dir, visited, steps + 1, max_steps)
-                }
-            } else {
-                None
-            }
+    fn right(&self) -> Option<Self> {
+        (self.x < WIDTH).then(|| Self {
+            x: self.x + 1,
+            y: self.y,
         })
-        .min()
+    }
+    fn up(&self) -> Option<Self> {
+        self.y.checked_sub(1).map(|y| Self { x: self.x, y })
+    }
+    fn down(&self) -> Option<Self> {
+        (self.y < HEIGHT).then(|| Self {
+            x: self.x,
+            y: self.y + 1,
+        })
+    }
+
+    pub fn successors(&self) -> Vec<Self> {
+        [self.left(), self.right(), self.up(), self.down()]
+            .into_iter()
+            .filter_map(|p| p)
+            .collect()
+    }
+
+    pub fn printable(&self) -> String {
+        format!("{},{}", self.x, self.y)
+    }
 }
 
-pub fn part_one(grid: &mut Grid, corrupt: &[Position]) -> usize {
-    for pos in &corrupt[..TTL] {
-        grid.inner[pos.to_flat_pos()] = b'#';
-    }
-    // Manual solve puts me in the low 300s
-    let mut max = 350;
-    walk(
-        grid,
-        0,
-        Direction::RIGHT,
-        &mut HashMap::with_capacity(grid.inner.len()),
-        0,
-        &mut max,
+pub fn parse(data: &[u8]) -> Vec<Position> {
+    data.split(|chr| b'\n'.eq(chr))
+        .map(|line| {
+            line.split_once(|chr| b','.eq(chr))
+                .map(|(x, y)| Position {
+                    x: common::stoi(x) as usize,
+                    y: common::stoi(y) as usize,
+                })
+                .unwrap()
+        })
+        .collect()
+}
+
+pub fn part_one(corrupted: &[Position]) -> usize {
+    dijkstra(
+        &Position { x: 0, y: 0 },
+        |p| {
+            p.successors()
+                .iter()
+                .filter_map(|&pos| (!corrupted.contains(&pos)).then_some((pos, 1)))
+                .collect::<Vec<_>>()
+        },
+        |p| GOAL.eq(p),
     )
-    .unwrap();
-    max
+    .unwrap()
+    .1
+}
+
+pub fn part_two(corrupted: &[Position]) -> String {
+    // shortcut: p1 taught us a route is guaranteed until TTL
+    for next in TTL.. {
+        if dijkstra(
+            &Position { x: 0, y: 0 },
+            |p| {
+                p.successors()
+                    .iter()
+                    .filter_map(|&pos| (!corrupted[..=next].contains(&pos)).then_some((pos, 1)))
+                    .collect::<Vec<_>>()
+            },
+            |p| GOAL.eq(p),
+        )
+        .is_none()
+        {
+            return corrupted[next].printable();
+        }
+    }
+    unreachable!()
 }
 
 pub fn main() {
     let data = common::read_file::<18>();
-    let (mut grid, corruption) = parse(&data);
-    println!("{}", part_one(&mut grid, &corruption));
+    let corruption = parse(&data);
+    println!("{}", part_one(&corruption[..TTL]));
+    println!("{}", part_two(&corruption));
 }
